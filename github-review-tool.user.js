99395ba9594e879edf67a5d5f4a543c27ddb62cc
Timo Sand <timo.j.sand@gmail.com>
2016-11-24 13:28:42 +0200

    Create github-review-tool.user.js

---
+++ github-review-tool.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Elisa GitHub Review Tool
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Easier PR's
+// @author       Timo Sand
+// @match        https://github.devcloud.elisa.fi/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $(".file > .file-header > .file-info > .user-select-contain:contains('bower_components'), .file > .file-header > .file-info > .user-select-contain:contains('node_modules'), .file > .file-header > .file-info > .user-select-contain:contains('.json')")
+        .parent()
+        .parent()
+        .parent()
+        .remove();
+})();
